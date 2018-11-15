aa93d5d3e82c87aa86cf74a0d5acb41418dfb623
Ma5ee2 <hussainmasee@gmail.com>
2018-11-15 19:59:11 +0000

    kept file helloworld

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
 import Shop from '@/components/Shop'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
     {
       path: '/',
       name: 'Shop',
