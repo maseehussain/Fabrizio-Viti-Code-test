0cbc4bceac4940d91f1dd463b1bbe4f8f6e3aa9a
Ma5ee2 <hussainmasee@gmail.com>
2018-11-15 20:08:45 +0000

    added the about page

---
+++ front-end/src/components/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <p>Information. This is the About Page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About' //this is the name of the component
+}
+</script>
+<style>
+</style>
--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Shop from '@/components/Shop'
+import Journal from '@/components/Journal'
+import About from '@/components/About'
 
 Vue.use(Router)
 
@@ -13,9 +15,19 @@ export default new Router({
       component: HelloWorld
     },
     {
-      path: '/',
+      path: '/Shop',
       name: 'Shop',
       component: Shop
+    },
+    {
+      path: '/Journal',
+      name: 'Journal',
+      component: Journal
+    },
+    {
+      path: '/About',
+      name: 'About',
+      component: About
     }
   ]
 })
