ed0102dca1c374570779bd0a1462c7c674f63810
Ma5ee2 <hussainmasee@gmail.com>
2018-11-15 19:46:37 +0000

    added content to the home page

---
+++ front-end/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Shop</h1>
+    <p>Information. This is the home page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home' //this is the name of the component
+}
+</script>
+<style>
+</style>
