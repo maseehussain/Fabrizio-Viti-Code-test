616da9acfb95dfaf5cf0cdaefb73cc07f38b314c
Ma5ee2 <hussainmasee@gmail.com>
2018-11-15 19:52:49 +0000

    added journal page

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Shop from '@/components/Shop'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Shop',
+      component: Shop
     }
   ]
 })
